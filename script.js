61950f94759c8500f51b97fce21e31460e28071f
Harsh Chaudhary <harshnimadia@gmail.com>
2024-10-10 10:47:03 +0530

    🐣 Pokemon cards - final commit

---
+++ script.js
@@ -0,0 +1,45 @@
+
+window.onload = function () {
+    // 1. get all the cards
+    var pack = document.getElementById("pokemon-pack");
+    pack.addEventListener("click", openPack);
+}
+
+
+function randomNumber(min, max) {
+    return Math.ceil(Math.random() * (max - min) + min);
+}
+
+
+function openPack() {
+    // alert("pack opened");
+
+
+    let cardsOpened = document.getElementById("pokemon-cards-opened");
+    while (cardsOpened.firstChild) {
+        cardsOpened.firstChild.remove();
+    }
+
+
+    for(let i = 0; i < 11; i++) {
+        let cardDiv = document.createElement("div");
+        cardDiv.classList.add("pokemon-card");
+
+
+        let cardImg = document.createElement("img");
+        cardImg.id = i;
+
+        let num = 1;
+        if(i == 10) {
+            num = randomNumber(1, 16);
+        }
+        else {
+            num = randomNumber(17, 102);
+        }
+
+        cardImg.src = "/pokemon-cards/base set (" + num.toString() + ").jpg";
+
+        cardDiv.appendChild(cardImg);
+        document.getElementById("pokemon-cards-opened").appendChild(cardDiv);
+    }
+}
